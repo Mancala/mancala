b2014413420a773e932034bc58a94bec25a9180d
Naved <navedz@gmail.com>
2010-11-01 18:14:06 +0200

    New project is created

---
+++ src/ut/sm/project/mancala/Main.java
@@ -0,0 +1,14 @@
+package ut.sm.project.mancala;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My java class created by Naved");
+
+	}
+
+}
